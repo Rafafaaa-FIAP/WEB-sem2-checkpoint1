0ee7d90f182f82556854cf2529df134d295e2fcc
Danillo Vieira <danillodesalesvieira@gmail.com>
2024-04-03 17:41:46 -0300

    feat: products page

--- src/routes/Products/Products.jsx
+++ src/routes/Products/Products.jsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import './products.css';
 
 function Products(){
 
@@ -7,8 +8,47 @@ function Products(){
 
     return(
         <>  
+          <section className="products">
+                <div className="products-title">
+                    <h1>Nossos Produtos</h1>
+                </div>
             
-            <h1>Products in Home</h1>
+                <section className="cards">
+                    <div className="card">
+                        <img src="./imgs/hoodie.jpg" alt="hoodie-img" className="card-img" />
+                        <h3>Moletom</h3>
+                        <p>Confortável e estiloso, une aconchego e moda.</p>
+                        <p className="price">R$ 100,00</p>
+                        <button>Comprar</button>
+                    </div>      
+
+                    <div className="card">
+                        <img src="./imgs/sneaker.jpg" alt="sneaker-img" className="card-img" />
+                        <h3>Tênis</h3>
+                        <p>Design inovador, combinando conforto e estilo.</p>
+                        <p className="price">R$ 100,00</p>
+                        <button>Comprar</button>
+                    </div> 
+
+                    <div className="card">
+                        <img src="./imgs/cap.jpg" alt="cap-img" className="card-img" />
+                        <h3>Boné</h3>
+                        <p>Sombra e estilo, perfeito para sua atitude.</p>
+                        <p className="price">R$ 100,00</p>
+                        <button>Comprar</button>
+                    </div> 
+
+                    <div className="card">
+                        <img src="./imgs/tshirt.jpg" alt="tshirt-img" className="card-img" />
+                        <h3>Camiseta</h3>
+                        <p>Versátil e respirável, ideal para toda ocasião.</p>
+                        <p className="price">R$ 100,00</p>
+                        <button>Comprar</button>
+                    </div> 
+
+
+                </section>
+            </section>
         </>
     )
 }
