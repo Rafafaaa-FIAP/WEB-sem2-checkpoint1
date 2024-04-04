012b5086ca4242c2270af08eab89fe5a81c99814
Rafafaaa <cristofalirafael@gmail.com>
2024-04-04 11:50:31 -0300

    fix: styles contact page

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,27 +1,24 @@
-import {} from "react"
+import { } from "react"
 
-import gmail from '../../assets/img/gmail.jpeg'
-import instagram from '../../assets/img/instagram.jpeg'
-import whatsapp from '../../assets/img/whatsapp.jpeg'
+import gmail from '../../assets/img/gmail.png'
+import instagram from '../../assets/img/instagram.png'
+import whatsapp from '../../assets/img/whatsapp.png'
 
 import './Contact.css'
 
-const Contact = ()=>{
+const Contact = () => {
     return (
-        <>
-            <h1>
-                Contato
-            </h1>
+        <div id='contact-page'>
+            <h1>Contato</h1>
+            <h2>Caso tenha alguma dúvida, entre em contato com a gente!</h2>
+            <p>E-mail: lojaesport@gmail.com</p>
+            <p>Whatsapp: (11) 99774-2213</p>
             <div className="imagens">
                 <img src={gmail} alt="gmail" />
                 <img src={instagram} alt="instagram" />
                 <img src={whatsapp} alt="Whatsapp" />
             </div>
-            
-
-            <p>Whatsapp: (11)99774-2213</p>
-            <p>E-mail: Lojaesport@gmail.com</p>
-        </>
+        </div>
     )
 }
 
