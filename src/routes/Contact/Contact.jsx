a3ae47cc1b1bd8951be9e336233b56e5da23e435
Jonataraff <jonatarafael209@gmail.com>
2024-04-03 22:45:06 -0300

    Feat: Contact page

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -1,9 +1,26 @@
 import {} from "react"
 
+import gmail from '../../assets/img/gmail.jpeg'
+import instagram from '../../assets/img/instagram.jpeg'
+import whatsapp from '../../assets/img/whatsapp.jpeg'
+
+import './Contact.css'
+
 const Contact = ()=>{
     return (
         <>
-            <h1>Contato</h1>
+            <h1>
+                Contato
+            </h1>
+            <div className="imagens">
+                <img src={gmail} alt="gmail" />
+                <img src={instagram} alt="instagram" />
+                <img src={whatsapp} alt="Whatsapp" />
+            </div>
+            
+
+            <p>Whatsapp: (11)99774-2213</p>
+            <p>E-mail: Lojaesport@gmail.com</p>
         </>
     )
 }
