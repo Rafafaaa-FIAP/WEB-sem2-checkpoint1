a05d643139c626c864db989b748612c13d885066
Rafafaaa <cristofalirafael@gmail.com>
2024-08-15 20:31:38 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./components/Navbar/Nav"
 import Footer from "./components/Footer/Footer"
 import "./index.css"
 
 
 function App() {
+
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    navigate('/Home');
+  }, []);
 
   return (
     <>
